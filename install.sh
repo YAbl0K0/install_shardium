6ce571374fb21e4f9f279450633253588968da96
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 09:46:20 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+apt install expect -y
+bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
+bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
+curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh && chmod +x installer.sh
+
+expect -c '
+#!/usr/bin/expect
+
+set timeout 5
+
+spawn ./installer.sh
+
+while 1 {
+    interact {
+        -nobuffer -o- "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" {send "\r"}
+        "What base directory should the node use (default ~/.shardeum):" {send "\r"}
+        "Do you want to run the web based Dashboard? (y/n):" {send "\r"}
+        "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" {send "8180"}
+        "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" {send "\r"}
+        "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" {send "\r"}
+        "This allows p2p commnication between nodes. Enter the first port (1025-65536) for p2p comminucation (default 9001):" {send "\r"}
+        "Enter the second port (1025-65536) for p2p comminucation (default 10001):" {send "\r"}
+        eof {break}
+    }
+}'
+
+rm ./installer.sh
+cd $HOME
+source $HOME/.shardeum/.env
+cd /root/.shardeum
+./shell.sh
+operator-cli start
+exit
+cd $HOME
+
+echo -e "\033[1;31;40mShardeum обновлен. Проверь количество токенов в explorer-sphinx.shardeum.org и делай стейк!\033[m"
