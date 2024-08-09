880c44b09e4696c52230b066959f456329247169
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 10:54:29 +0300

    Create install_2.sh

---
+++ install_2.sh
@@ -0,0 +1,15 @@
+
+expect "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" 
+send "8180\r"
+
+expect "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" 
+send "\r"
+
+expect "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" 
+send "\r"
+
+expect "This allows p2p commnication between nodes. Enter the first port (1025-65536) for p2p comminucation (default 9001):" 
+send "\r"
+
+expect "Enter the second port (1025-65536) for p2p comminucation (default 10001):" 
+send "\r"
