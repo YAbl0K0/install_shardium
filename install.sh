73c9e85ddaf76e878c33b87400c189e71d9c5694
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 14:50:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh && chmod +x installer.sh
 
+read -sp "Введите пароль для Dashboard: " PASSWORD
+
 expect -c '
 set timeout 10
 
@@ -11,7 +13,7 @@ expect {
     "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" {send "y\r"; exp_continue}
     "What base directory should the node use (default ~/.shardeum):" {send "\r"; exp_continue}
     "Do you want to run the web based Dashboard? (Y/n):" {send "\r"; exp_continue}
-    "Set the password to access the Dashboard:" {send "pass\r"; exp_continue}
+    "Set the password to access the Dashboard:" {send "'"$PASSWORD"'\r"; exp_continue}
     "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" {send "8180\r"; exp_continue}
     "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" {send "\r"; exp_continue}
     "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" {send "\r"; exp_continue}
@@ -20,6 +22,4 @@ expect {
     eof {break}
 }
 '
-
-bash <(curl -s https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh)
 
