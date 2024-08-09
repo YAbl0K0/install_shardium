dc757fddd23688bc5547b8f738bc26d701bad11b
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 10:53:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,24 +19,7 @@ send "\r"
 expect "Do you want to run the web based Dashboard? (y/n):" 
 send "\r"
 
-# Ожидание ввода пароля вручную
 expect "Set the password to access the Dashboard:"
-expect_user -re ".*\n"
-
-expect "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" 
-send "8180\r"
-
-expect "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" 
-send "\r"
-
-expect "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" 
-send "\r"
-
-expect "This allows p2p commnication between nodes. Enter the first port (1025-65536) for p2p comminucation (default 9001):" 
-send "\r"
-
-expect "Enter the second port (1025-65536) for p2p comminucation (default 10001):" 
-send "\r"
 
 expect eof
 '
