2220b2e2d8568248586a8e23e7151d158c42c59c
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 10:21:44 +0300

    Update Install.sh v0.1.2

--- install.sh
+++ install.sh
@@ -5,39 +5,39 @@ bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
 curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh && chmod +x installer.sh
 
-expect << EOF
+expect -c '
 set timeout 5
 
 spawn ./installer.sh
 
-expect "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:"
+expect "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" 
 send "\r"
 
-expect "What base directory should the node use (default ~/.shardeum):"
+expect "What base directory should the node use (default ~/.shardeum):" 
 send "\r"
 
-expect "Do you want to run the web based Dashboard? (y/n):"
+expect "Do you want to run the web based Dashboard? (y/n):" 
 send "\r"
 
-expect "Set the password to access the Dashboard:"
+expect "Set the password to access the Dashboard:" 
 
-expect "Enter the port (1025-65536) to access the web based Dashboard (default 8080):"
+expect "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" 
 send "8180\r"
 
-expect "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):"
+expect "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" 
 send "\r"
 
-expect "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):"
+expect "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" 
 send "\r"
 
-expect "This allows p2p commnication between nodes. Enter the first port (1025-65536) for p2p comminucation (default 9001):"
+expect "This allows p2p commnication between nodes. Enter the first port (1025-65536) for p2p comminucation (default 9001):" 
 send "\r"
 
-expect "Enter the second port (1025-65536) for p2p comminucation (default 10001):"
+expect "Enter the second port (1025-65536) for p2p comminucation (default 10001):" 
 send "\r"
 
 expect eof
-EOF
+'
 
 rm ./installer.sh
 cd $HOME
