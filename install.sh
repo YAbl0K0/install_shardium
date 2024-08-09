d26cf326e80f392e3ecce7c883eb981b33779222
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 14:52:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,3 +23,12 @@ expect {
 }
 '
 
+rm ./installer.sh
+cd $HOME/.shardeum/ && ./shell.sh
+operator-cli start
+exit
+
+source $HOME/.shardeum/.env
+cd $HOME
+
+echo -e "\033[1;31;40mShardeum установлен. Проверь количество токенов в explorer-sphinx.shardeum.org и делай стейк!\033[m"
