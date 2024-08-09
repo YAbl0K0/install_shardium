63dca2ff050990e59387deb6175fbffeeaed0488
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 16:09:31 +0300

    Update install_2.sh

--- install_2.sh
+++ install_2.sh
@@ -4,14 +4,14 @@
 read -sp "Введите пароль: " NEW_PASSWORD
 
 # Переход в директорию Shardeum и запуск оболочки
-expect -c "
+expect -c '
 spawn bash -c \"cd $HOME/.shardeum/ && ./shell.sh\"
 
 expect \"node@b16999ac25f0:~#\" {send \"operator-cli start\r\"; exp_continue}
 expect \"node@b16999ac25f0:~#\" {send \"operator-cli gui set password $NEW_PASSWORD\r\"; exp_continue}
 expect \"node@b16999ac25f0:~#\" {send \"operator-cli gui restart\r\"; exp_continue}
 expect \"node@b16999ac25f0:~#\" {send \"exit\r\"; exp_continue}
-"
+'
 
 # Удаление установочного скрипта и обновление окружения
 rm ./installer.sh
