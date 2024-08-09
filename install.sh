c6247f4772fcaadd31fdf62088032732417c4f47
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 10:24:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,9 @@ send "\r"
 expect "Do you want to run the web based Dashboard? (y/n):" 
 send "\r"
 
-expect "Set the password to access the Dashboard:" 
+# Ожидание ввода пароля вручную
+expect "Set the password to access the Dashboard:"
+expect_user -re ".*\n"
 
 expect "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" 
 send "8180\r"
