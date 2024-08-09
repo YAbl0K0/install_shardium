3e8e36bd86c6a93f6fd4711c144ca598d1435b51
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 16:36:37 +0300

    Update install_2.sh

--- install_2.sh
+++ install_2.sh
@@ -3,7 +3,6 @@
 # Запрашиваем пароль у пользователя
 read -sp "Введите пароль: " NEW_PASSWORD
 MY_PASS=$NEW_PASSWORD
-echo  # Перевод строки после ввода пароля
 
 # Выполняем команды внутри контейнера Docker
 docker exec -i shardeum-dashboard operator-cli start
