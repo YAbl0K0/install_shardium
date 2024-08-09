f5f91137792d9fa2c281e05e8f8935dc481f7e67
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 10:00:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,27 +5,37 @@ bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
 curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh && chmod +x installer.sh
 
-expect -c 
-#!/usr/bin/expect
-
+expect -c '
 set timeout 5
 
 spawn ./installer.sh
 
-while 1 {
-    interact {
-        -nobuffer -o- "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" {send "\r"}
-        "What base directory should the node use (default ~/.shardeum):" {send "\r"}
-        "Do you want to run the web based Dashboard? (y/n):" {send "\r"}
-        "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" {send "8180"}
-        "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" {send "\r"}
-        "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" {send "\r"}
-        "This allows p2p commnication between nodes. Enter the first port (1025-65536) for p2p comminucation (default 9001):" {send "\r"}
-        "Enter the second port (1025-65536) for p2p comminucation (default 10001):" {send "\r"}
-        eof {break}
-    }
-}
+expect "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" 
+send "\r"
 
+expect "What base directory should the node use (default ~/.shardeum):" 
+send "\r"
+
+expect "Do you want to run the web based Dashboard? (y/n):" 
+send "\r"
+
+expect "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" 
+send "8180\r"
+
+expect "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" 
+send "\r"
+
+expect "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" 
+send "\r"
+
+expect "This allows p2p commnication between nodes. Enter the first port (1025-65536) for p2p comminucation (default 9001):" 
+send "\r"
+
+expect "Enter the second port (1025-65536) for p2p comminucation (default 10001):" 
+send "\r"
+
+expect eof
+'
 rm ./installer.sh
 cd $HOME
 source $HOME/.shardeum/.env
