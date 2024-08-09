033bd28b1d31e044599e530863bed3559e37db65
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 12:01:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh
 curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh && chmod +x installer.sh
 
 expect -c '
-set timeout 5
+set timeout 10
 
 spawn ./installer.sh
 
@@ -15,8 +15,8 @@ while 1 {
         "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" {send "y\r"}
         "What base directory should the node use (default ~/.shardeum):" {send "\r"}
         "Do you want to run the web based Dashboard? (Y/n):" {send "\r"}
-        "Set the password to access the Dashboard:" {interact}
-        "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" {send "8180\r"}
+        "Set the password to access the Dashboard:" {interact exp_continue}
+        "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" {send "8180\r" exp_continue}
         "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" {send "\r"}
         "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" {send "\r"}
         "This allows p2p communication between nodes. Enter the first port (1025-65536) for p2p communication (default 9001):" {send "\r"}
@@ -24,6 +24,7 @@ while 1 {
         eof {break}
     }
 }
+log_user 1
 '
 
 rm ./installer.sh
