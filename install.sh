d61c3c77ff2dbb03df73eb5b194492f36f80e9a8
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 14:39:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,18 +20,5 @@ expect {
     eof {break}
 }
 '
+bash <(curl -s https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh)
 
-read -sp "Введите пароль: " NEW_PASSWORD
-
-cd $HOME/.shardeum/ && ./shell.sh
-
-operator-cli start
-operator-cli gui set password "$NEW_PASSWORD"
-operator-cli gui restart
-exit
-
-rm ./installer.sh
-source $HOME/.shardeum/.env
-cd $HOME
-
-echo -e "\033[1;31;40mShardeum установлен. Проверь количество токенов в explorer-sphinx.shardeum.org и делай стейк!\033[m"
