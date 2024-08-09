3af2ed6a060c6b4d1e57dd86e146a75ba280b5a2
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-09 14:23:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,45 @@ set timeout 5
 
 spawn ./installer.sh
 
-while 1 {
-    expect {
-        "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" {send "y\r"}
-        "What base directory should the node use (default ~/.shardeum):" {send "\r"}
-        "Do you want to run the web based Dashboard? (Y/n):" {send "\r"}
-        "Set the password to access the Dashboard:" {interact}
-    }
-}
+expect "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:"
+send "y\r"
+
+expect "What base directory should the node use (default ~/.shardeum):"
+send "\r"
+
+expect "Do you want to run the web based Dashboard? (Y/n):"
+send "\r"
+
+# Ввод пароля вручную
+expect "Set the password to access the Dashboard:"
+interact
+
+# Продолжение после ручного ввода пароля
+expect "Enter the port (1025-65536) to access the web based Dashboard (default 8080):"
+send "8180\r"
+
+expect "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):"
+send "\r"
+
+expect "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):"
+send "\r"
+
+expect "This allows p2p communication between nodes. Enter the first port (1025-65536) for p2p communication (default 9001):"
+send "\r"
+
+expect "Enter the second port (1025-65536) for p2p communication (default 10001):"
+send "\r"
+
+expect eof
 '
 
-bash <(curl -s https://raw.githubusercontent.com/YAbl0K0/install_shardium/main/install_2.sh)
+rm ./installer.sh
+cd $HOME
+source $HOME/.shardeum/.env
+cd /root/.shardeum
+./shell.sh
+operator-cli start
+exit
+cd $HOME
+
+echo -e "\033[1;31;40mShardeum установлен. Проверь количество токенов в explorer-sphinx.shardeum.org и делай стейк!\033[m"
