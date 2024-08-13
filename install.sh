406bbee83c45e91704d9259d74fc470e89d7daf9
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-13 13:30:04 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,6 +29,5 @@ while 1 {
 rm ./installer.sh
 source $HOME/.shardeum/.env
 cd $HOME
-docker exec -i shardeum-dashboard operator-cli start
 
 echo -e "\033[1;31;40mShardeum установлен. Проверь количество токенов в explorer-sphinx.shardeum.org и делай стейк!\033[m"
