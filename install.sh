c8f2d79f5779154cd23345d09565611030f65a77
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-10-01 20:42:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+docker image prune -a -f
+docker volume prune -f
+docker rm -f shardeum-dashboard
+
 apt install expect -y
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
-curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/dev/installer.sh && chmod +x installer.sh
+curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh && chmod +x installer.sh
 
 expect -c '
 set timeout 5
@@ -15,8 +19,8 @@ while 1 {
         "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" {send "y\r"; exp_continue}
         "What base directory should the node use (default ~/.shardeum):" {send "\r"; exp_continue}
         "Do you want to run the web based Dashboard? (Y/n):" {send "\r"; exp_continue}
-        "Set the password to access the Dashboard:" {send "123QwErTy*\r"; exp_continue}
-        "Enter the port (1025-65536) to access the web based Dashboard (default 8180):" {send "8180\r"; exp_continue}
+        "Set the password to access the Dashboard:" {send "111111\r"; exp_continue}
+        "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" {send "8180\r"; exp_continue}
         "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" {send "\r"; exp_continue}
         "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" {send "\r"; exp_continue}
         "This allows p2p communication between nodes. Enter the first port (1025-65536) for p2p communication (default 9001):" {send "\r"; exp_continue}
